8bf03537ab8bcf5e18a60f8d54f3c540db0b9f03
mrs504aa <mrs504aa@hotmail.com>
2022-02-18 17:40:21 +0800

    Add error management.

--- src/MyToolBox.jl
+++ src/MyToolBox.jl
@@ -5,6 +5,8 @@ function ArrayNormalization(A::Vector{<:Real})
 end
 
 function SignalCut(TargetSignal::Vector{<:Real}, ReferenceSignal::Vector{<:Real}, Window::Vector{<:Real})
+    length(Window) == 2 || throw(ArgumentError("Invalid length of (Window = $Window), should be 2."))
+    length(TargetSignal) == length(ReferenceSignal) || throw(ArgumentError("Length of TargetSignal should be same as the length of ReferenceSignal."))
     S = Vector{Bool}(undef, length(ReferenceSignal))
     for i in 1:length(ReferenceSignal)
         S[i] = true * (ReferenceSignal[i] >= Window[1]) * (ReferenceSignal[i] <= Window[2])
